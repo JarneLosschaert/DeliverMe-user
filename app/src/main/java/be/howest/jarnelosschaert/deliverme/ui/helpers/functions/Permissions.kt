30f29099f6a31047cefadd7e400fa398a555d6cc
Loesje <jarne.losschaert@student.howest.be>
2024-01-06 16:18:19 +0100

    fixed bug notifcations

--- app/src/main/java/be/howest/jarnelosschaert/deliverme/ui/helpers/functions/Permissions.kt
+++ app/src/main/java/be/howest/jarnelosschaert/deliverme/ui/helpers/functions/Permissions.kt
@@ -3,8 +3,10 @@ package be.howest.jarnelosschaert.deliverme.ui.helpers.functions
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
@@ -34,6 +36,7 @@ fun HandleLocationPermissions(onPermission: (Boolean) -> Unit) {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun HandleNotificationPermissions() {
     val context = LocalContext.current
